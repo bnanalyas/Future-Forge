f5dc4ee9c72f3687927a6cfc1f5c2ad19c26e9fc
bnanalyas <bnanalyas@gmail.com>
2025-12-10 12:03:33 +0300

    Create api.py

---
+++ backend/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    # نستقبل الملف (حتى لو ما نستخدمه الآن)
+    uploaded_file = request.files.get("file")
+
+    # نحاكي تحليل الذكاء الاصطناعي
+    risk_score = random.randint(1, 100)
+
+    if risk_score > 80:
+        risk_level = "High"
+    elif risk_score > 50:
+        risk_level = "Medium"
+    else:
+        risk_level = "Low"
+
+    return jsonify({
+        "risk_score": risk_score,
+        "risk_level": risk_level
+    })
+
+if __name__ == "__main__":
+    app.run()
